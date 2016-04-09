14c13b11613c9fbc266f1eebb66e24e2b466b02e
kaceymilus <kaceymilus@gmail.com>
2016-04-09 20:04:10 +0000

    Add about route

---
+++ controllers/about.js
@@ -0,0 +1,9 @@
+// About page 
+
+var About = {
+    
+    index: function(req, res) {
+        res.send('This should be the about page.');
+    }
+    
+}; 
--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ var Router = {
 
         // Home Page 
         app.get('/', controllers.Home.index);
+        
+        // About Page
+        app.get('/about', controllers.About.index);
 
         // Users
         app.get('/users', controllers.Users.index);
